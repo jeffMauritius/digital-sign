eaec9adabace0dc4af46646ee30186ffa99d7ec9
jeff <jfroussel75@gmail.com>
2024-09-13 18:36:37 +0400

    cleaning code

--- src/components/admin-panel/collapse-menu-button.tsx
+++ src/components/admin-panel/collapse-menu-button.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 import { useState } from "react"
-import { ChevronDown, Dot, LucideIcon, Minus } from "lucide-react"
+import { ChevronDown, LucideIcon, Minus } from "lucide-react"
 import { useTranslations } from "next-intl"
 
 import { cn } from "@/lib/utils"
