eeab94f0ad9aa815065d256d6b04471b03b35985
jeff <jfroussel75@gmail.com>
2024-09-25 18:23:49 +0400

    fix build error

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -16,9 +16,6 @@ import { ContentLayout } from "@/components/admin-panel/content-layout"
 import BreadcrumbComponent from "@/components/breadcrumbComponent"
 import { useTranslations } from "next-intl"
 
-export const description =
-  "A settings page. The settings page has a sidebar navigation and a main content area. The main content area has a form to update the store name and a form to update the plugins directory. The sidebar navigation has links to general, security, integrations, support, organizations, and advanced settings."
-
 const ProfilePage = () => {
   const t = useTranslations()
   return (
