898103dab1c0a267d54980d199839967f6cfb670
jeff <jfroussel75@gmail.com>
2024-09-27 11:37:47 +0400

    add prisma database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,32 +8,16 @@ import { ModeToggle } from "@/components/mode-toggle"
 export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="z-[50] sticky top-0 w-full bg-background/95 border-b backdrop-blur-sm dark:bg-black/[0.6] border-border/40">
-        <div className="container h-20 flex items-center">
-          <Link
-            href="/"
-            className="hidden justify-start items-center hover:opacity-85 transition-opacity duration-300"
-          >
-            <Image src={"/logo.png"} alt={"logo"} width={250} height={60} />
-          </Link>
-          <nav className="ml-auto flex items-center gap-2">
-            <Button
-              variant="outline"
-              size="icon"
-              className="rounded-full w-8 h-8 bg-background"
-              asChild
-            >
-              <Link href="https://google.com">
-                <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
-              </Link>
-            </Button>
-            <ModeToggle />
-          </nav>
-        </div>
-      </header>
       <main className="min-h-[calc(100vh-57px-97px)] flex-1">
         <div className="container relative pb-10">
           <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-6">
+            <Image
+              src={"/logo.png"}
+              alt={""}
+              width={200}
+              height={200}
+              className="pb-5"
+            />
             <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
               Digital signature
             </h1>
